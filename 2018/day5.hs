526e69395230e1d56cf82ea5e8b09bef990b7ad6
Henrik Andersson <he.and03a@gmail.com>
2018-12-05 23:06:39 +0100

    18-12-05

---
+++ 2018/day5.hs
@@ -0,0 +1,28 @@
+module Main where
+
+import Data.Char
+import Data.List
+
+willReact :: Char -> Char -> Bool
+willReact c c'
+  | (isLower c) && (isUpper c') && (toUpper c) == c' = True
+  | (isUpper c) && (isLower c') && (toUpper c') == c = True
+  | otherwise = False
+
+reactPolymer :: String -> String
+reactPolymer polymer =
+  let polymer' = foldr go [] polymer
+  in if polymer' == polymer then polymer else reactPolymer polymer'
+  where
+    go x [] = [x]
+    go x (y:ys)
+      | willReact x y = ys
+      | otherwise = x:y:ys
+
+main :: IO ()
+main = do
+  polymer <- readFile "day5_input"
+  let reactedPolymer = length $ reactPolymer polymer
+      bestReaction = minimum $ map (length . reactPolymer . (\c -> filter ((/=(toLower c)) . toLower) polymer)) ['a'..'z']
+  putStrLn $ "Part 1: " ++ (show reactedPolymer)
+  putStrLn $ "Part 2: " ++ (show bestReaction)
