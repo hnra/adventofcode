b5df5f79ad7904e44ec65a2d0e3ce8c691927ba6
Henrik Andersson <he.and03a@gmail.com>
2018-12-10 00:26:18 +0100

    60s -> 0.04s and part 2 in 7s

--- 2018/day9.hs
+++ 2018/day9.hs
@@ -3,12 +3,11 @@
 module Main where
 
 import qualified Data.Sequence as S
-import qualified Data.HashMap.Lazy as H
-import Debug.Trace
+import qualified Data.IntMap.Lazy as I
 
 data GameState = GameState {
-  circle :: S.Seq Int, -- Make infinite?
-  scores :: H.HashMap Int Int,
+  circle :: S.Seq Int,
+  scores :: I.IntMap Int,
   currentMarble :: Int,
   nextMarble :: Int,
   currPlayer :: Int
@@ -17,47 +16,33 @@ data GameState = GameState {
 players :: Int
 players = 452
 
-lastMarble :: Int
-lastMarble = 71250
-
 initGame :: GameState
-initGame = GameState (S.fromList [0, 2, 1]) H.empty 2 3 3
+initGame = GameState (S.fromList [0, 2, 1]) I.empty 1 3 3
 
-currMarbleIndex :: GameState -> Int
-currMarbleIndex (GameState circle _ cm _ _) =
-  case S.findIndexL (==cm) circle of
-    Just i -> i
-    Nothing -> error "Invalid gamestate"
-
-insertIndex :: GameState -> Int
-insertIndex gs@(GameState circle _ _ _ _) =
-  let cmi = currMarbleIndex gs
-      cl  = S.length circle
-  in if | cmi + 2 == cl -> cl
-        | otherwise -> (cmi + 2) `mod` cl
-
-nextState :: GameState -> GameState
-nextState gs@(GameState circle scores cm nm cp)
-  | cm >= lastMarble = gs
+nextState :: Int -> GameState -> GameState
+nextState lm gs@(GameState circle scores ci nm cp)
+  | nm > lm = gs
   | mod nm 23 == 0 =
-      let cmi = currMarbleIndex gs
-          rmi = ((cmi - 7) `mod` S.length circle)
-          rm  = circle `S.index` rmi
-          scores' = H.insertWith (+) cp (nm+rm) scores
+      let rmi = (ci-7) `mod` (S.length circle)
+          rm = S.index circle rmi
+          scores' = I.insertWith (+) cp (nm+rm) scores
           circle' = S.deleteAt rmi circle
-          cm' = circle' `S.index` rmi
-          cp' = if (cp+1) `mod` (players+1) == 0 then 1 else cp+1
-      in GameState circle' scores' cm' (nm+1) cp'
+          cp' = (cp+1) `mod` players
+      in GameState circle' scores' rmi (nm+1) cp'
   | otherwise =
-      let cmi = currMarbleIndex gs
-          ii = insertIndex gs
+      let cl = S.length circle
+          ii = if ci + 2 == cl then cl else (ci + 2) `mod` cl
           circle' = S.insertAt ii nm circle
-          cp' = if (cp+1) `mod` (players+1) == 0 then 1 else cp+1
-      in GameState circle' scores nm (nm+1) cp'
+          cp' = (cp+1) `mod` players
+      in GameState circle' scores ii (nm+1) cp'
 
-playGame :: GameState -> Int
-playGame gs
-  | (nextState gs) == gs = maximum $ map snd $ H.toList $ scores gs
-  | otherwise = playGame (nextState gs)
+playGame :: Int -> GameState -> Int
+playGame lm gs =
+  let ns = nextState lm gs
+  in if | ns == gs -> maximum $ map snd $ I.toList $ scores gs
+        | otherwise -> playGame lm ns
 
-main = putStrLn $ show $ playGame initGame
+main :: IO ()
+main = do
+  putStrLn $ "Part 1: " ++ (show $ playGame 71250 initGame)
+  putStrLn $ "Part 2: " ++ (show $ playGame (71250*100) initGame)
