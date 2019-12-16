6d508115a01873a748a0d2ca297941d3c6b1e350
Henrik Andersson <he.and03a@gmail.com>
2019-12-16 19:27:52 +0100

    🧦 Day 5 🧦

--- 2019/day5.hs
+++ 2019/day5.hs
@@ -6,58 +6,101 @@ import Data.List.Split
 import qualified Data.Map.Lazy as M
 import Data.Map.Lazy (Map, (!))
 
--- a PC has a program counter, memory, inputs, and outputs
-data PC = PC Integer (Map Integer String) [Integer] [Integer] deriving Show
+type Memory = Map Integer String
+type Input = [Integer]
+type Output = [Integer]
+type IP = Integer
+data PC = PC IP Memory Input Output deriving Show
 
 getInputs :: IO [String]
 getInputs = splitOn "," <$> readFile "inputs/day5"
 
 -- Load parameter based on mode.
-load :: Char -> String -> PC -> Integer
-load '0' p (PC _ mem _ _) = read $ mem ! (read p)
-load '1' p (PC _ _ _ _) = read p
+load :: Char -> String -> Memory -> Integer
+load '0' p mem = read $ mem ! (read p)
+load '1' p _   = read p
+load _ _ _     = undefined
 
 -- Read the parameters of a three parameter instruction operation.
-getParams :: (String, String, String, String) -> PC -> (Integer, Integer, Integer)
-getParams ((m1:o:p:[]), p1, p2, p3) pc = (load m1 p1 pc, load '0' p2 pc, read p3)
-getParams ((m2:m1:o:p:[]), p1, p2, p3) pc = (load m1 p1 pc, load m2 p2 pc, read p3)
-getParams ((_:m2:m1:o:p:[]), p1, p2, p3) pc = (load m1 p1 pc, load m2 p2 pc, read p3)
-getParams (_, p1, p2, p3) pc = (load '0' p1 pc, load '0' p2 pc, read p3)
+getParam3 :: (String, String, String, String) -> Memory -> (Integer, Integer, Integer)
+getParam3 ((m1:_:_:[]),      p1, p2, p3) mem = (load m1  p1 mem, load '0' p2 mem, read p3)
+getParam3 ((m2:m1:_:_:[]),   p1, p2, p3) mem = (load m1  p1 mem, load m2  p2 mem, read p3)
+getParam3 ((_:m2:m1:_:_:[]), p1, p2, p3) mem = (load m1  p1 mem, load m2  p2 mem, read p3)
+getParam3 (_,                p1, p2, p3) mem = (load '0' p1 mem, load '0' p2 mem, read p3)
+
+-- Read the parameters of a two parameter instruction operation.
+getParam2 :: (String, String, String) -> Memory -> (Integer, Integer)
+getParam2 ((m1:_:_:[]),      p1, p2) mem = (load m1  p1 mem, load '0' p2 mem)
+getParam2 ((m2:m1:_:_:[]),   p1, p2) mem = (load m1  p1 mem, load m2  p2 mem)
+getParam2 ((_:m2:m1:_:_:[]), p1, p2) mem = (load m1  p1 mem, load m2  p2 mem)
+getParam2 (_,                p1, p2) mem = (load '0' p1 mem, load '0' p2 mem)
 
 -- Read the parameter of a single parameter instruction operation.
-getParam :: (String, String) -> PC -> Integer
-getParam ((m:o:p:[]), p1) pc = load m p1 pc
-getParam (_, p) pc = load '0' p pc
+getParam :: (String, String) -> Memory -> Integer
+getParam ((m:_:_:[]), p1) mem = load m   p1 mem
+getParam (_, p)           mem = load '0' p  mem
 
 -- Execute the PC once and indiciate when END instruction has been reached.
-exec :: PC -> (PC, Bool)
-exec p@(PC pc mem ins outs) =
-  case last (mem ! pc) of
-    '9' -> (p, False)
-    '1' -> (opexec (+), True)
-    '2' -> (opexec (*), True)
-    '3' -> (PC (pc + 2) (M.insert (read $ mem ! (pc + 1)) (show $ head ins) mem) (tail ins) outs, True)
-    '4' -> ((PC (pc + 2) mem ins ((getParam (mem ! pc, mem ! (pc+1)) p):outs)), True)
-    _ -> undefined
-  where opexec :: (Integer -> Integer -> Integer) -> PC
-        opexec op =
-          let (p1, p2, p3) = getParams (mem ! pc, mem ! (pc+1), mem ! (pc+2), mem ! (pc+3)) p
-          in (PC (pc + 4) (M.insert p3 (show $ op p1 p2) mem) ins outs)
+exec :: PC -> Either Integer PC
+exec (PC ip mem ins outs)
+  | length instr > 1 && drop (length instr - 3) instr == "99" = Left (head outs)
+  | otherwise =
+    Right $ case last instr of
+      '1' -> opexec (+)
+      '2' -> opexec (*)
+      '3' -> PC (ip + 2) (M.insert (read $ mem ! (ip + 1)) (show $ head ins) mem) (tail ins) outs
+      '4' -> PC (ip + 2) mem ins ((getParam (mem ! ip, mem ! (ip+1)) mem):outs)
+      '5' -> ifelse (/=0)
+      '6' -> ifelse (==0)
+      '7' -> cmp (<)
+      '8' -> cmp (==)
+      _ -> undefined
+  where
+    instr :: String
+    instr = mem ! ip
+
+    at :: Integer -> String
+    at = (!) mem
+
+    p2s :: (String, String, String)
+    p2s = (at ip, at $ ip + 1, at $ ip + 2)
+
+    p3s :: (String, String, String, String)
+    p3s = (at ip, at $ ip + 1, at $ ip + 2, at $ ip + 3)
+
+    ifelse :: (Integer -> Bool) -> PC
+    ifelse f
+      | f p1      = PC p2 mem ins outs
+      | otherwise = PC (ip + 3) mem ins outs
+      where (p1, p2) = getParam2 p2s mem
+
+    cmp :: (Integer -> Integer -> Bool) -> PC
+    cmp f = PC (ip + 4) (M.insert p3 (if f p1 p2 then "1" else "0") mem) ins outs
+      where (p1, p2, p3) = getParam3 p3s mem
+
+    opexec :: (Integer -> Integer -> Integer) -> PC
+    opexec op = (PC (ip + 4) (M.insert p3 (show $ op p1 p2) mem) ins outs)
+      where (p1, p2, p3) = getParam3 p3s mem
 
 -- Run a PC until END.
-run :: PC -> PC
+run :: PC -> Integer
 run pc =
   case exec pc of
-    (pc', True) -> run pc'
-    (pc', False) -> pc'
+    Right pc' -> run pc'
+    Left out -> out
 
-count :: [a] -> [(Integer, a)]
-count [] = []
-count (a:as) = foldl (\acc@((i, _):_) a -> (i+1,a):acc) [(0, a)] as
+enumerate :: [a] -> [(Integer, a)]
+enumerate xs = go xs 0
+  where
+    go :: [a] -> Integer -> [(Integer, a)]
+    go []     _ = []
+    go (a:as) i = (i, a) : (go as (i+1))
 
+main :: IO ()
 main = do
   instrs <- getInputs
-  let mem = (M.fromList . count) instrs
-      (PC _ _ _ outs) = run (PC 0 mem [1] [])
-  print $ "Part 1: " ++ (show $ head outs)
-
+  let mem = (M.fromList . enumerate) instrs
+      o1 = run (PC 0 mem [1] [])
+      o2 = run (PC 0 mem [5] [])
+  print $ "Part 1: " ++ (show $ o1)
+  print $ "Part 2: " ++ (show $ o2)
