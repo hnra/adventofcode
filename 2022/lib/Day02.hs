b6d302024a525f2ed46961c1f306923e246891d4
Henrik Andersson <h@hnra.dev>
2022-12-02 10:04:59 +0100

    Day 2 part 2

--- 2022/lib/Day02.hs
+++ 2022/lib/Day02.hs
@@ -4,7 +4,7 @@ import Utilities
 import Data.Text (Text)
 import qualified Data.Text as T
 
-data Move = Rock | Paper | Scissors
+data Move = Rock | Paper | Scissors
     deriving (Eq, Show)
 
 fromStr :: Text -> Move
@@ -17,6 +17,16 @@ fromStr "C" = Scissors
 fromStr _ = error "Invalid input"
 
 type Strategy = (Move, Move)
+
+parseLine :: Text -> Strategy
+parseLine l = (fromStr theirs, fromStr mine)
+    where
+        theirs:mine:_ = T.words l
+
+strategies :: IO [Strategy]
+strategies = do
+    input <- getInput "02"
+    return $ map parseLine (T.lines input)
 
 moveScore :: Move -> Int
 moveScore Rock = 1
@@ -34,21 +44,32 @@ step (x, y)
     | x == y = 3 + moveScore y
     | otherwise = error "Unhandled input"
 
-parseLine :: Text -> Strategy
-parseLine l = (fromStr theirs, fromStr mine)
-    where
-        theirs:mine:_ = T.words l
+p1 :: [Strategy] -> Int
+p1 = foldr (\s score -> score + step s) 0
 
-strategies :: IO [Strategy]
-strategies = do
-    input <- getInput "02"
-    return $ map parseLine (T.lines input) 
+data Outcome = Lose | Draw | Win
+    deriving (Eq, Show)
 
-d1 :: [Strategy] -> Int
-d1 = foldr (\s score -> score + step s) 0
+toOutcome :: Move -> Outcome
+toOutcome Rock = Lose
+toOutcome Paper = Draw
+toOutcome Scissors = Win
+
+step2 :: (Move, Outcome) -> Int
+step2 (Rock, Lose) = step (Rock, Scissors)
+step2 (Rock, Win) = step (Rock, Paper)
+step2 (Paper, Lose) = step (Paper, Rock)
+step2 (Paper, Win) = step (Paper, Scissors)
+step2 (Scissors, Lose) = step (Scissors, Paper)
+step2 (Scissors, Win) = step (Scissors, Rock)
+step2 (x, Draw) = step (x, x)
+
+p2 :: [Strategy] -> Int
+p2 = foldr (\(theirs, mine) score -> score + step2 (theirs, toOutcome mine)) 0
 
 day2 :: IO ()
 day2 = do
     input <- strategies
     putStrLn "⭐⭐ Day 2 ⭐⭐"
-    putStrLn $ "Part 1: " ++ show (d1 input)
+    putStrLn $ "Part 1: " ++ show (p1 input)
+    putStrLn $ "Part 2: " ++ show (p2 input)
