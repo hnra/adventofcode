d9243ec04da9a47c5b06149116eec38c729251ce
Henrik Andersson <h@hnra.dev>
2022-12-02 07:27:21 +0100

    Day2 part 1

--- 2022/app/Main.hs
+++ 2022/app/Main.hs
@@ -1,8 +1,11 @@
 module Main where
 
 import Day1
+import Day2
 
 main :: IO ()
 main = do
     day1
+    putStrLn ""
+    day2
     putStrLn ""
---
+++ 2022/lib/Day2.hs
@@ -0,0 +1,54 @@
+module Day2 (day2) where
+
+import Utilities
+import Data.Text (Text)
+import qualified Data.Text as T
+
+data Move = Rock | Paper | Scissors
+    deriving (Eq, Show)
+
+fromStr :: Text -> Move
+fromStr "X" = Rock
+fromStr "A" = Rock
+fromStr "Y" = Paper
+fromStr "B" = Paper
+fromStr "Z" = Scissors
+fromStr "C" = Scissors
+fromStr _ = error "Invalid input"
+
+type Strategy = (Move, Move)
+
+moveScore :: Move -> Int
+moveScore Rock = 1
+moveScore Paper = 2
+moveScore Scissors = 3
+
+step :: Strategy -> Int
+step (Rock, Paper) = 6 + moveScore Paper
+step (Rock, Scissors) = moveScore Scissors
+step (Paper, Rock) = moveScore Rock
+step (Paper, Scissors) = 6 + moveScore Scissors
+step (Scissors, Rock) = 6 + moveScore Rock
+step (Scissors, Paper) = moveScore Paper
+step (x, y)
+    | x == y = 3 + moveScore y
+    | otherwise = error "Unhandled input"
+
+parseLine :: Text -> Strategy
+parseLine l = (fromStr theirs, fromStr mine)
+    where
+        theirs:mine:_ = T.words l
+
+strategies :: IO [Strategy]
+strategies = do
+    input <- getInput "02"
+    return $ map parseLine (T.lines input) 
+
+d1 :: [Strategy] -> Int
+d1 = foldr (\s score -> score + step s) 0
+
+day2 :: IO ()
+day2 = do
+    input <- strategies
+    putStrLn "⭐⭐ Day 2 ⭐⭐"
+    putStrLn $ "Part 1: " ++ show (d1 input)
