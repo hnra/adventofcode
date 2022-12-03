7d29f8f0007f5dcd3b180366821e73cd56701ed5
Henrik Andersson <h@hnra.dev>
2022-12-03 10:57:20 +0100

    Use intersect for day 3

--- 2022/lib/Day03.hs
+++ 2022/lib/Day03.hs
@@ -1,6 +1,7 @@
 module Day03 (day3) where
 
 import Data.Char (isLower)
+import Data.List (intersect)
 
 type Rucksack2 = (String, String)
 
@@ -15,22 +16,19 @@ priority c
     | isLower c = fromEnum c - 96
     | otherwise = fromEnum c - 38
 
-commonChar :: (String, String) -> Char
-commonChar (ss, ss') = head [s | s <- ss, s' <- ss', s == s']
-
 p1 :: [Rucksack2] -> Int
-p1 = sum . map (priority . commonChar)
+p1 = sum . map (priority . head . uncurry intersect)
 
-type Rucksack3 = (String, String, String)
+type Rucksack3 = [String]
 
 part2input :: [String] -> [Rucksack3]
 part2input = groupBy3
     where
-        groupBy3 (a:b:c:rest) = (a, b, c) : groupBy3 rest
+        groupBy3 (a:b:c:rest) = [a, b, c] : groupBy3 rest
         groupBy3 _ = []
 
 common3 :: Rucksack3 -> Char
-common3 (xs, ys, zs) = head [x | x <- xs, y <- ys, x == y, z <- zs, y == z]
+common3 rs = head $ foldr1 intersect rs
 
 p2 :: [Rucksack3] -> Int
 p2 = sum . map (priority . common3)
