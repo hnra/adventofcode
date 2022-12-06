b115d2dcd4999bbceb8b3a7dde2fca090d3d00ca
Henrik Andersson <h@hnra.dev>
2022-12-06 07:26:11 +0100

    Day 6

--- 2022/app/Main.hs
+++ 2022/app/Main.hs
@@ -5,10 +5,11 @@ import Day02 (day2)
 import Day03 (day3)
 import Day04 (day4)
 import Day05 (day5)
+import Day06 (day6)
 import Data.List (intersperse)
 
 main :: IO ()
 main = do
     let
-        days = [day1, day2, day3, day4, day5]
+        days = [day1, day2, day3, day4, day5, day6]
     sequence_ $ intersperse (putStrLn "") days
---
+++ 2022/lib/Day06.hs
@@ -0,0 +1,18 @@
+module Day06 where
+
+import Utilities (getInput)
+import Data.List (nub)
+import qualified Data.Text as T
+
+p :: Int -> String -> Int
+p cnt input = go cnt input
+    where
+        go i input@(_:rest)
+            | (length . nub . take cnt) input == cnt = i
+            | otherwise = go (i + 1) rest
+
+day6 = do
+    input <- T.unpack <$> getInput "06"
+    putStrLn "⭐⭐ Day 6 ⭐⭐"
+    putStrLn $ "Part 1: " ++ show (p 4 input)
+    putStrLn $ "Part 2: " ++ show (p 14 input)
