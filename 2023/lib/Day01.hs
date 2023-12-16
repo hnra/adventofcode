ddffa3db587fc7d0113e99559e01f20f8c023ba3
Henrik Andersson <h@hnra.dev>
2023-12-16 13:13:42 +0100

    2023 - Day 1 - Part 2

--- 2023/lib/Day01.hs
+++ 2023/lib/Day01.hs
@@ -2,6 +2,7 @@ module Day01 where
 
 import Utilities (getLinesS)
 import Data.Char (isDigit)
+import Data.Text (replace, pack, unpack, Text)
 
 day1input :: IO [String]
 day1input = getLinesS "01"
@@ -9,13 +10,37 @@ day1input = getLinesS "01"
 digits :: String -> String
 digits = filter isDigit
 
-calibrationValues :: [String] -> [Int]
-calibrationValues = map (read .firstAndLast)
+calibrations :: [String] -> [Int]
+calibrations = map (read . firstAndLast)
   where firstAndLast l = head l:last l:""
+
+replacers :: [Text -> Text]
+replacers =
+  [ replace "one" "o1e"
+  , replace "two" "t2o"
+  , replace "three" "th3ee"
+  , replace "four" "fo4r"
+  , replace "five" "f5ve"
+  , replace "six" "s6x"
+  , replace "seven" "se7en"
+  , replace "eight" "ei8ht"
+  , replace "nine" "ni9e"
+  , replace "zero" "ze0o"
+  ]
+
+-- | Converts a string containg (possibly overlapping) digits as words and
+-- digit chars to a string of only digits.
+-- @wordsToDigits "eightwo3sevenine" == "82379"@
+wordsToDigits :: String -> String
+wordsToDigits line = digits (unpack tline)
+  where 
+    tline = foldr ($) (pack line) replacers
 
 day1 :: IO ()
 day1 = do
   putStrLn "⭐⭐ Day 1 ⭐⭐"
   input <- day1input
-  let p1 = (sum . calibrationValues . map digits) input
+  let p1 = (sum . calibrations . map digits) input
+  let p2 = (sum . calibrations . map wordsToDigits) input
   putStrLn $ "Part 1: " ++ show p1
+  putStrLn $ "Part 2: " ++ show p2
