aff1ec2d0a68d66046f848146ece85c052d7bacd
Henrik Andersson <h@hnra.dev>
2024-12-10 16:49:17 +0100

    Use PathBuf for path

--- 2024/src/main.rs
+++ 2024/src/main.rs
@@ -1,4 +1,5 @@
 use std::fs;
+use std::path::PathBuf;
 
 use day01::day01;
 
@@ -6,7 +7,8 @@ mod day01;
 
 fn main() {
     println!("--- Day 1 ---");
-    let contents = fs::read_to_string("inputs/day01").expect("Cannot read input");
+    let contents = fs::read_to_string(PathBuf::from_iter(["inputs", "day01"].iter()))
+        .expect("Cannot read input");
     let (d01_p1, d01_p2) = day01(&contents);
     println!("Part 1: {}", d01_p1);
     println!("Part 2: {}", d01_p2);
