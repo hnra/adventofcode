9ba23eb7cd4c9a34bbaf6089fe1bc58a55a750e4
Henrik Andersson <h@hnra.dev>
2024-12-23 10:44:07 +0100

    🦀 Day 4 - Part 1 🦀

---
+++ 2024/src/day04.rs
@@ -0,0 +1,136 @@
+pub fn day04(input: &str) -> (usize, usize) {
+    let width = input.find("\n").unwrap();
+    let words = input.replace("\n", "");
+    let height = words.len() / width;
+
+    let p1 = cnt_horizontal_and_diag(&words, width)
+        + cnt_horizontal_and_diag(&rotate(&words, width), height);
+
+    (p1, 0)
+}
+
+fn cnt_horizontal_and_diag(words: &str, width: usize) -> usize {
+    let height = words.len() / width;
+    let mut cnt = 0;
+    let chars = words.as_bytes();
+    for (i, c) in chars.iter().enumerate() {
+        if *c != b'X' && *c != b'S' {
+            continue;
+        }
+
+        let row = i / width;
+        let col = i % width;
+
+        if col + 4 <= width {
+            if is_xmas(&chars[i..i + 4]) {
+                cnt += 1;
+            }
+
+            if row + 4 <= height {
+                let vertical_str = vec![
+                    chars[i],
+                    chars[i + width + 1],
+                    chars[i + 2 * (width + 1)],
+                    chars[i + 3 * (width + 1)],
+                ];
+                if is_xmas(&vertical_str) {
+                    cnt += 1;
+                }
+            }
+        }
+    }
+
+    cnt
+}
+
+fn is_xmas(s: &[u8]) -> bool {
+    match s {
+        b"XMAS" => true,
+        b"SAMX" => true,
+        _ => false,
+    }
+}
+
+fn rotate(s: &str, width: usize) -> String {
+    let height = s.len() / width;
+    let mut new_str_vec = vec![' '; s.len()];
+
+    for (i, c) in s.char_indices() {
+        let row = i / width;
+        let col = i % width;
+
+        let new_row = col;
+        let new_col = row.abs_diff(height - 1);
+
+        let new_i = new_row * height + new_col;
+        new_str_vec[new_i] = c;
+    }
+
+    new_str_vec.into_iter().collect()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn example() {
+        let example_text = "MMMSXXMASM
+MSAMXMSMSA
+AMXSXMAAMM
+MSAMASMSMX
+XMASAMXAMM
+XXAMMXXAMA
+SMSMSASXSS
+SAXAMASAAA
+MAMMMXMMMM
+MXMXAXMASX";
+        let (p1, _) = day04(example_text);
+        assert_eq!(18, p1);
+    }
+
+    #[test]
+    fn test_is_xmas() {
+        assert!(is_xmas(b"XMAS"));
+        assert!(is_xmas(b"SAMX"));
+        assert!(!is_xmas(b"SMX"));
+    }
+
+    #[test]
+    fn horizontal() {
+        let example_text = "XMAS
+SAMX";
+        let (p1, _) = day04(example_text);
+        assert_eq!(2, p1);
+    }
+
+    #[test]
+    fn vertical() {
+        let example_text = "X00S
+M00A
+A00M
+S00X";
+        let (p1, _) = day04(example_text);
+        assert_eq!(2, p1);
+    }
+
+    #[test]
+    fn diagonal() {
+        let example_text = "X00S
+0MA0
+0MA0
+X00S";
+        let (p1, _) = day04(example_text);
+        assert_eq!(2, p1);
+    }
+
+    #[test]
+    fn diagonal_rev() {
+        let example_text = "S00X
+0AM0
+0AM0
+S00X";
+        let (p1, _) = day04(example_text);
+        assert_eq!(2, p1);
+    }
+}
--- 2024/src/main.rs
+++ 2024/src/main.rs
@@ -4,10 +4,12 @@ use std::path::PathBuf;
 use day01::day01;
 use day02::day02;
 use day03::day03;
+use day04::day04;
 
 mod day01;
 mod day02;
 mod day03;
+mod day04;
 
 fn main() {
     println!("--- Day 1 ---");
@@ -30,4 +32,11 @@ fn main() {
     let (d03_p1, d03_p2) = day03(&contents);
     println!("Part 1: {}", d03_p1);
     println!("Part 2: {}", d03_p2);
+
+    println!("\n--- Day 4 ---");
+    contents = fs::read_to_string(PathBuf::from_iter(["inputs", "day04"].iter()))
+        .expect("Cannot read input");
+    let (d04_p1, d04_p2) = day04(&contents);
+    println!("Part 1: {}", d04_p1);
+    println!("Part 2: {}", d04_p2);
 }
